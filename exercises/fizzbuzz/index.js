299ed90b076769066f0d42a678cce4a34c7d46c3
Jason Gutierrez <jgutierrez@americanpayroll.org>
2017-11-21 23:35:04 -0600

    <code>Updated syntax for fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -16,9 +16,9 @@ const fizzBuzz = n => {
   for(let i = 1; i <= n; i++) {
     (i % 15 === 0) ?
       console.log("fizzbuzz") : 
-       (i % 3 === 0) ? 
-         console.log("fizz") :
-           (i % 5 === 0) ? console.log("buzz") :
+       (i % 5 === 0) ? 
+         console.log("buzz") :
+           (i % 3 === 0) ? console.log("fizz") :
              console.log(i);
   }
 }
