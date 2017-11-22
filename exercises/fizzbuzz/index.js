a10c0ddd9ba6e6b2802e66a1202cd43db601af15
Jason Gutierrez <jgutierrez@americanpayroll.org>
2017-11-21 22:47:10 -0600

    <code> Add Solution to fizzBuzz with Ternary

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,7 +12,15 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) { 
+const fizzBuzz = n => {
+  for(let i = 1; i <= n; i++) {
+    (i % 15 === 0) ?
+      console.log("fizzbuzz") : 
+       (i % 3 === 0) ? 
+         console.log("fizz") :
+           (i % 5 === 0) ? console.log("buzz") :
+             console.log(i);
+  }
 }
 
 module.exports = fizzBuzz;
